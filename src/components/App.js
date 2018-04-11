f82e0ae0f6ec262c8be2566bdb9d3c425898af93
bigjek <ivapira@mail.ru>
2018-04-11 15:53:56 +0300

    add article list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Article from './Article';
+import ArticleList from './ArticleList';
 import articles from '../fixture'
 function App (){
 	return (
       <div>
     	<h1> Мой проект </h1>
-      	<Article article={articles[0]} foo="bar" flag />
+      	<ArticleList articles={articles} />
       </div>
     )
 }
