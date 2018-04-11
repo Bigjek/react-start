3f31e28871ac9be2d21ec48ab3ee0f78f80b4183
bigjek <ivapira@mail.ru>
2018-04-11 21:30:24 +0300

    add css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ArticleList from './ArticleList';
 import articles from '../fixture'
+import 'bootstrap/dist/css/bootstrap.css'
 function App (){
 	return (
-      <div>
-    	<h1> Мой проект </h1>
+      <div className='container' >  
+        <div className='jumbotron'>
+    	    <h1 className='display-3'> Мой проект </h1>            
+        </div>
       	<ArticleList articles={articles} />
       </div>
     )
--- src/components/Article.js
+++ src/components/Article.js
@@ -18,17 +18,21 @@ class Article extends Component {
     render(){
     const {article} = this.props
     //Добавление условий для отображения
-    const body = this.state.isOpen && <section>{article.text}</section>;	
+    const body = this.state.isOpen && <section className='cart-text'>{article.text}</section>;	
     return (
-    	<div className="hello"> 
-            <h2>
-                {article.title}
-                <button onClick={this.closeClick}>
-                    {this.state.isOpen ? 'close' : 'open'}
-                </button>
-            </h2>
-            {body}
-            <h3>date creation: {(new Date(article.date)).toDateString()}</h3>
+    	<div className="card mx-auto" style = {{width:'50%'}}>
+            <div className='card-header'>
+                <h2>
+                    {article.title}
+                    <button onClick={this.closeClick} className='btn btn-primary btn-lg float-right'>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+            </div>
+            <div className='card-body'>
+                <h6 className='card-subtitle text-muted'>date creation: {(new Date(article.date)).toDateString()}</h6>
+                {body}
+            </div>
       	</div>
     )}
     closeClick = () => {
