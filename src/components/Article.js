5249bb8aecc2b1014efad4a73b9757fd28a7eeca
bigjek <ivapira@mail.ru>
2018-04-11 13:59:38 +0300

    change state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,41 @@
-import React from 'react';
-function Article (props){
-    const {article} = props
-    const body = <section>{article.text}</section>;	
-    console.log('props', props);
+import React, {Component} from 'react';
+
+class Article extends Component {
+    //начальное состояние
+    // constructor(props){
+    //     super(props)
+
+    //     this.state = {
+    //         isOpen: true
+    //     }
+        
+    //     this.closeClick = closeClick.bind(this)
+    // }
+    state = {
+        isOpen: true
+    }
+
+    render(){
+    const {article} = this.props
+    //Добавление условий для отображения
+    const body = this.state.isOpen && <section>{article.text}</section>;	
     return (
     	<div className="hello"> 
             <h2>
                 {article.title}
-                <button onClick={closeClick}>close</button>
+                <button onClick={this.closeClick}>
+                    {this.state.isOpen ? 'close' : 'open'}
+                </button>
             </h2>
             {body}
             <h3>date creation: {(new Date(article.date)).toDateString()}</h3>
       	</div>
-    )
-}
-function closeClick(){
-    console.log('close');
+    )}
+    closeClick = () => {
+       console.log('close'); 
+       this.setState({
+           isOpen: !this.state.isOpen
+       })
+    }
 }
 export default Article
